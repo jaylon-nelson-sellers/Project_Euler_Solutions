f32c5efda0e5d6953cc558e0d47b804894bfcad8
jaylon-nelson-sellers <jcns@uw.edu>
2020-02-19 19:42:07 -0800

    Forgot to add to the commit with 4 and 5

--- src/Problem4.java
+++ src/Problem4.java
@@ -1,4 +1,81 @@
-package PACKAGE_NAME;
-
+/**
+ * Largest palindrome product
+ *
+ * Problem 4
+ * A palindromic number reads the same both ways. The largest palindrome made
+ * from the product of two 2-digit numbers is 9009 = 91 × 99.
+ *
+ * Find the largest palindrome made from the product of two 3-digit numbers.
+ *
+ * Brainstorming:
+ * I can brute force this i believe. a nested for loop will probably do the job.
+ *
+ * Result:
+ * After some debugging, I got the correct answer of 913 x 993 = 906609.
+ * I did this by using two methods: one to go through each value through 1-999
+ * and one to check if the product of the values is a palindrome.
+ * Rate it a 2/10. Solved on first attempt.
+ */
 public class Problem4 {
+
+    public static void main(String[] args) {
+        int[] temp = (palindromeFinder(1000));
+        System.out.println("Value 1:" + temp[0] + "Value 2:" + temp[1]);
+        int palindrome = temp[0] * temp[1];
+        System.out.println("Largest Palindrome is: " + palindrome);
+    }
+
+    public static int[] palindromeFinder(int max) {
+        int[] maxValue = new int[2];
+        maxValue[0] = 0;
+        maxValue[1] = 0;
+        for (int i = 1; i < max; i++) {
+            for (int j = 1; j < max; j++) {
+                int tempNumber = (i * j);
+                String tempString =  Integer.toString(tempNumber);
+                if (palindromeChecker(tempString)) {
+                    if (maxValue[0] * maxValue[1] < i * j) {
+                        maxValue[0] = i;
+                        maxValue[1] = j;
+                    }
+                }
+            }
+        }
+        return maxValue;
+    }
+
+    public static boolean palindromeChecker(String toBeChecked) {
+        int length = toBeChecked.length();
+        if (length % 2 == 0) {
+            int end = length - 1;
+            for (int i = 0; i < length; i++) {
+                if (toBeChecked.charAt(i) == toBeChecked.charAt(end)) {
+                    end--;
+                    continue;
+                } else {
+                    return false;
+                }
+            }
+            return true;
+
+        } else if (length == 1) {
+            return true;
+        } else {
+            int end = length - 1;
+            for (int i = 0; i < length; i++) {
+                if (i == (length / 2) + 1) {
+                    end--;
+                    continue;
+                }
+
+                if (toBeChecked.charAt(i) == toBeChecked.charAt(end)) {
+                    end--;
+                    continue;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return false;
+    }
 }
--- src/Problem5.java
+++ src/Problem5.java
@@ -1,4 +1,56 @@
-package PACKAGE_NAME;
+import java.math.BigDecimal;
+/**Smallest multiple
 
+ Problem 5
+ 2520 is the smallest number that can be divided by each of the numbers from 1
+ to 10 without any remainder.
+
+ What is the smallest positive number that is evenly divisible by all of the
+ numbers from 1 to 20?
+
+ Brainstorming: I think another brute force attempt will work. Using a nested
+ for loop for all values.
+ First Attempt got a really large number, I used the debugger to test 2520 and
+ it got the correct answer (returning positive for each value) but the given
+ number is massive at 2.32792561E8
+ *
+ * I got the correct answer? but the computer rounded up which confused me. I
+ * rounded it down, and the answer was correct. Not really happy with it because
+ * I had to intervene with the answer and didn't get a direct answer.
+ * difficulty: 2/10
+ * first attemp submition
+ */
 public class Problem5 {
+
+    public static void main(String[] args) {
+        //Got a weird number? Rounded it down
+        //System.out.println(findDivisible());
+
+        int p = 232792560;
+
+        for (int i = 1; i <= 20; i++) {
+            System.out.println(p / i);
+        }
+    }
+
+    public static double findDivisible() {
+        boolean testCondition = false;
+        double i = 1;
+        while (!testCondition) {
+            for (int j = 1; j <= 20; j++) {
+                if (i % j == 0) {
+                    testCondition = true;
+                } else {
+                    testCondition = false;
+                    break;
+                }
+            }
+            i++;
+        }
+
+        System.out.println("2) " + BigDecimal.valueOf(i).toPlainString());
+        return i;
+    }
+
 }
+
